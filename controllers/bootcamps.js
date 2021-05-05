1172dee9fd96aa3756437949af5deaea97a2ec60
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-05-05 12:20:40 +0530

    Update bootcamps.js

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,9 +8,7 @@ exports.getBootCamps = async (req, res, next) => {
 			data: bootcamp
 		});
 	} catch (err) {
-		res.status(400).json({
-			success: false
-		});
+		next(err);
 	}
 };
 
@@ -22,9 +20,7 @@ exports.getBootCamp = async (req, res, next) => {
 			data: bootcamp
 		});
 	} catch (err) {
-		res.status(400).json({
-			success: false
-		});
+		next(err);
 	}
 };
 
