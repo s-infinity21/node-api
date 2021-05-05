2fc7f3befa1910b303865360cf778069f40f9105
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-05-05 12:17:44 +0530

    Add files via upload

---
+++ middleware/error.js
@@ -0,0 +1,10 @@
+const errHandler = (err, req, res, next) => {
+	console.log(err.stack);
+
+	res.status(500).json({
+		success: false,
+		error: err.message
+	});
+};
+
+module.exports = errHandler;
