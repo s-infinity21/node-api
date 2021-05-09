d69473b5234a88850a77505b4b624b44c5926e69
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-05-10 00:00:38 +0530

    Update error.js

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,31 @@
+const ErrResponse = require('../utility/errorResponse');
 const errHandler = (err, req, res, next) => {
-	console.log(err.stack);
+	let error = { ...err };
+	error.message = err.message;
 
-	res.status(500).json({
+	console.log(err);
+
+	// moongoose bad objectid
+	if (err.name === 'CastError') {
+		const message = `Bootcamp not found with id ${err.value}`;
+		error = new ErrResponse(message, 404);
+	}
+
+	// moongoose dublicate field
+	if (err.code === 11000) {
+		const message = Object.values(err.error).map(val => val.message);
+		error = new ErrResponse(message, 400);
+	}
+
+	// moongoose validation error
+	if (err.name === 'ValidationError') {
+		const message = `Bootcamp not found with id ${err.value}`;
+		error = new ErrResponse(message, 404);
+	}
+
+	res.status(error.statusCode || 500).json({
 		success: false,
-		error: err.message
+		error: error.message || 'Server Error'
 	});
 };
 
